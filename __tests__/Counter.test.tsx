7156fa0864c2979e86b8ee8513d2ce5434d03b09
jccabarlo <jessecabarlo@gmail.com>
2025-05-26 08:44:04 +0800

    style: format Counter test file

--- __tests__/Counter.test.tsx
+++ __tests__/Counter.test.tsx
@@ -1,18 +1,18 @@
-import Counter from '@/components/Counter';
-import { fireEvent, render } from '@testing-library/react-native';
+import Counter from "@/components/Counter";
+import { fireEvent, render } from "@testing-library/react-native";
 
-describe('Counter', () => {
-  it('should display initial count of zero', () => {
+describe("Counter", () => {
+  it("should display initial count of zero", () => {
     const { getByTestId } = render(<Counter />);
-    expect(getByTestId('counter-value').props.children).toBe(0);
+    expect(getByTestId("counter-value").props.children).toBe(0);
   });
 
-  it('should increment count when increment button is pressed', () => {
+  it("should increment count when increment button is pressed", () => {
     const { getByTestId } = render(<Counter />);
-    const incrementButton = getByTestId('increment-button');
-    
+    const incrementButton = getByTestId("increment-button");
+
     fireEvent.press(incrementButton);
-    
-    expect(getByTestId('counter-value').props.children).toBe(1);
+
+    expect(getByTestId("counter-value").props.children).toBe(1);
   });
 });
